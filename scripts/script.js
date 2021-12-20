ffd80759cd68a1fbc756a0e2341f9484cfb0e40f
mpdev2311 <marcospdev2311gmail.com>
2021-12-20 16:30:16 -0300

    minha oitava  aula

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,18 @@
 const $header = document.querySelector("header");
+const $logo = document.querySelectorAll('.logo')[0];
 window.addEventListener('scroll',toggleHeader,false);
 function toggleHeader() {
     if (window.pageYOffset > 60 &&  $header.classList.contains('max-header')) {             
         $header.classList.remove('max-header');
-        $header.classList.add('min-header');       
-    } else if (window.pageYOffset <= 60 &&  $header.classList.contains('min-header')) {                
+        $header.classList.add('min-header');
+        $logo.firstElementChild .setAttribute('src','imgs/logo2.png'); 
+        $logo.classList.remove('max-logo');
+        $logo.classList.add('min-logo');    
+    } else if (window.pageYOffset <= 60 &&  $header.classList.contains('min-header')) {                        
         $header.classList.add('max-header');
         $header.classList.remove('min-header');
+        $logo.firstElementChild .setAttribute('src','imgs/logo1.png'); 
+        $logo.classList.add('max-logo');
+        $logo.classList.remove('min-logo');    
     }
 }
