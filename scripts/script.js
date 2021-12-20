c217b47a23134aece5975f83a1e75faaea00b87a
mpdev2311 <marcospdev2311gmail.com>
2021-12-20 18:06:05 -0300

    minha noma  aula

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const $header = document.querySelector("header");
 const $logo = document.querySelectorAll('.logo')[0];
+const $navBar = document.querySelectorAll('.nav-bar')[0];
 window.addEventListener('scroll',toggleHeader,false);
 function toggleHeader() {
     if (window.pageYOffset > 60 &&  $header.classList.contains('max-header')) {             
@@ -7,12 +8,16 @@ function toggleHeader() {
         $header.classList.add('min-header');
         $logo.firstElementChild .setAttribute('src','imgs/logo2.png'); 
         $logo.classList.remove('max-logo');
-        $logo.classList.add('min-logo');    
+        $logo.classList.add('min-logo');
+        $navBar.classList.remove('max-nav'); 
+        $navBar.classList.add('min-nav');  
     } else if (window.pageYOffset <= 60 &&  $header.classList.contains('min-header')) {                        
         $header.classList.add('max-header');
         $header.classList.remove('min-header');
         $logo.firstElementChild .setAttribute('src','imgs/logo1.png'); 
         $logo.classList.add('max-logo');
-        $logo.classList.remove('min-logo');    
+        $logo.classList.remove('min-logo');
+        $navBar.classList.remove('max-nav'); 
+        $navBar.classList.add('min-nav');     
     }
 }
